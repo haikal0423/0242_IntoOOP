d66eee38d6aabf6379d978c62e90d1a48d86f441
haikal0423 <mhaikal23azka@gmail.com>
2024-04-23 16:30:44 +0700

    Menambahkan void input, cout masukan panjang, cin panjang, cout masukan lebar, cin lebar, float hitung luas dan return panjang x lebar

--- OOPPemdas2/OOPPemdas2.cpp
+++ OOPPemdas2/OOPPemdas2.cpp
@@ -8,4 +8,14 @@ private:
 	float panjang, lebar;
 public:
 	float Luas;
+	void input() { // methode input persegi panjang
+		cout << "Masukan Panjangnya = ";
+		cin >> panjang;
+		cout << "Masukan Lebarnya = ";
+		cin >> lebar;
+	}
+	float hitungLuas() {
+		return panjang * lebar;
+	}
+
 };
